32f7b331ba8f645637a1ea4e14f4996f657cacee
vishal prasad <73818798+vishaaaal23@users.noreply.github.com>
2023-11-20 20:02:59 +0530

    Update Random.js

--- src/components/Random.js
+++ src/components/Random.js
@@ -22,9 +22,6 @@ const Random = () => {
     {
         loading ? (<Spinner/>) : (<img src= {gif} width="450" />)
     }
-
-      
-
       <button onClick={() => fetchData()}
       className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
 
